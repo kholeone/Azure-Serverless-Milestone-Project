b35282b709b435cee9798297c6a04100dbf86377
Ubuntu <Kholeone@vm-severless-project.sih4vneaghhuxh1mn13idf1oga.zx.internal.cloudapp.net>
2020-12-10 14:00:35 +0000

    initial commit

---
+++ azure-serverless-milestone-project/service-one/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import requests
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+     letterstring = requests.get('http://localhost:7071/api/service-two').text
+
+     nostring = requests.get('http://localhost:7071/api/service-three').text
+
+    username = ""
+    for i in range(5):
+        username += letterstring[i]
+        username += nostring[i]
+
+
